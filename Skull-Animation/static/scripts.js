8ede65a31a57324220cbdb8fee8b00213741cdb4
Yann Figueiredo <yann.fabricio@hotmail.com>
2021-02-17 18:02:56 -0300

    Tentativas de fazer funcionar frustadas

--- Skull-Animation/static/scripts.js
+++ Skull-Animation/static/scripts.js
@@ -34,40 +34,7 @@ function iniciar(){
     } );
 }*/
 function iniciar(){
-    var cena = new THREE.Scene();
-    var LARGURA = window.innerWidth;
-    var ALTURA = window.innerHeight;
-    //var loader = new THREE.GLTFLoader();
-
-    cena.background = new THREE.Color(0x002123);
-
-    //cena.setClearColor(0x002123);
-
-    //1º parâmetro é o campo de visão, 2º é o tamanho da janela (para os objetos não ficarem destorcidos),
-    //3º é a distância da lente com relação ao elemento, 4º é o limite de distância de renderização
-    var camera = new THREE.PerspectiveCamera(45, LARGURA/ALTURA, 1, 1000);
-
-    camera.position.z = 50; //Profundidade
-
-    //antilias é para evitar serrilhado no objeto renderizado
-    var renderer = new THREE.WebGLRenderer({antilias:true}); //Instanciar objeto para renderização
-
-    renderer.setSize(LARGURA, ALTURA);
-
-    //Aplica o objeto no body para renderização
-    document.body.appendChild(renderer.domElement);
-
-    //loader.load( 'models/scene.gltf', function ( gltf ) {
-    //    cena.add(gltf.scene);
-    //})
-
-    function animate(){
-        renderer.render(cena, camera);
-
-        requestAnimationFrame(animate);
-    }
-
-    animate();
+    
 }
 
 window.onload = iniciar;
