a725b72617d16d9b7d29e04d30eb667cdfda6fd9
YannFigueiredo <yann.fabricio@hotmail.com>
2021-02-18 12:12:32 -0300

    Projeto finalizado

--- Skull-Animation/static/scripts.js
+++ Skull-Animation/static/scripts.js
@@ -6,13 +6,7 @@ function iniciar(){
     scene.background = new THREE.Color(0x434246);
     
     camera = new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight,1,1000);
-    //camera.rotation.y = 45/180*Math.PI;
-    //camera.position.x = window.innerWidth/2;
-    //camera.position.y = window.innerHeight/2;
     camera.position.z = 11;
-    
-    //controls = new THREE.OrbitControls(camera);
-    //controls.addEventListener('change', renderer);
     
     hlight = new THREE.AmbientLight (0xECECF4,3);
     scene.add(hlight);
@@ -21,23 +15,7 @@ function iniciar(){
     directionalLight.position.set(0,1,0);
     directionalLight.castShadow = true;
     scene.add(directionalLight);
-    /*
-    light = new THREE.PointLight(0xc4c4c4,10);
-    light.position.set(0,300,500);
-    scene.add(light);
     
-    light2 = new THREE.PointLight(0xc4c4c4,10);
-    light2.position.set(500,100,0);
-    scene.add(light2);
-    
-    light3 = new THREE.PointLight(0xc4c4c4,10);
-    light3.position.set(0,100,-500);
-    scene.add(light3);
-    
-    light4 = new THREE.PointLight(0xc4c4c4,10);
-    light4.position.set(-500,300,500);
-    scene.add(light4);
-    */
     renderer = new THREE.WebGLRenderer({antialias:true});
     renderer.setSize(window.innerWidth,window.innerHeight);
     
@@ -54,7 +32,7 @@ function iniciar(){
     var controle = true;
     
     function animate() {
-        renderer.render(scene,camera);
+        renderer.render(scene, camera);
 
         if(skull){
             skull.rotation.x += 0.02;
